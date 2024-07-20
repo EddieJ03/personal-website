014cca36fae651f7a204fbbfd30ecb0e91bc1b7e
Edward Jin <57547638+EddieJ03@users.noreply.github.com>
2024-07-19 22:42:12 -0700

    Update Intro.js

--- pages/components/Intro.js
+++ pages/components/Intro.js
@@ -20,7 +20,7 @@ function Intro({ darkMode, setDarkMode }) {
           impactful software and enjoy learning new tools & technologies.
         </h3>
         <a
-          href="https://drive.google.com/file/d/1AZh0XIZOOZzUGBWS44C6xN30ERQvq_3s/view"
+          href="https://drive.google.com/file/d/1nQZW_OWJ8B3GCX7A13E09BcVu25EmHuh/view"
           className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white bg-blue-400 rounded-lg hover:bg-blue-500 focus:ring-4 focus:ring-blue-300 dark:bg-blue-800 dark:hover:bg-blue-900"
         >
           RESUME
